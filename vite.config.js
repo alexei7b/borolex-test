8b4dfed01a8af3df8922f852a979053341f2b433
Alex <alexei_7b@mail.ru>
2026-03-25 16:50:27 +0200

    new test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: "/borolex-test/",
     build: {
+        outDir: 'docs',
         rolldownOptions: {
             input: {
                 main: resolve(import.meta.dirname, 'index.html'),
