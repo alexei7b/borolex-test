602073f8ac1f6057058a47b3c3c80c73c03ccdab
Alex <alexei_7b@mail.ru>
2026-03-25 15:48:16 +0200

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { constants } from 'node:buffer'
+import { dirname, resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: "/borolex-test/",
+    build: {
+        rolldownOptions: {
+            input: {
+                main: resolve(import.meta.dirname, 'index.html'),
+                portfolio: resolve(import.meta.dirname, 'portfolio.html'),
+                contacts: resolve(import.meta.dirname, 'contacts.html'),
+                services: resolve(import.meta.dirname, 'services.html')
+            },
+        },
+    },
+})
