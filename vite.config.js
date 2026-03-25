5e9e7d06346beb4217d32dde81d1664b3c1e979d
Alex <alexei_7b@mail.ru>
2026-03-25 17:12:52 +0200

    test commit 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: process.env.NODE_ENV === 'production'
         ? '/borolex-test/'
-        : '/',
+        : '/borolex.com/',
     build: {
         outDir: 'docs',
         rolldownOptions: {
